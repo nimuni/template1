a802dc6e0ec4b0674ba6b5f0bf24035bbc6d977f
nimuni <kym9788@gmail.com>
2022-06-15 17:15:14 +0900

    chore: 잘못된 설치 취소

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -4,6 +4,32 @@ var router = express.Router();
 var testRouter = require('./test');
 var userRouter = require('./users');
 
+// graphql
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+router.get('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public', 'index.html')); 

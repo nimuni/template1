c5fe230dcaeeef29a6f07c44ee7cc07ac06c2750
nimuni <kym9788@gmail.com>
2022-06-15 13:27:53 +0900

    Initial commit

---
+++ be/public/precache-manifest.fa894a4606a612aa3ee00f18406cb806.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f33e8709b1d9f735bc",
+    "url": "/css/app.44427119.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e1e4de587bef6200970b9ef220b92fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b543a82cfa55a62585e7",
+    "url": "/js/about.d5fce027.js"
+  },
+  {
+    "revision": "b5f33e8709b1d9f735bc",
+    "url": "/js/app.c2325eaf.js"
+  },
+  {
+    "revision": "a12b9cf176a365967e41",
+    "url": "/js/chunk-vendors.aec8814c.js"
+  },
+  {
+    "revision": "8953b545e34a626bae8bcc61a379e284",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
